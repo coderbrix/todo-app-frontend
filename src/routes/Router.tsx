edb6a17bd02d7b1a7c56d172567fc325eccf27a6
MDRAHUL47 <rahulahames@gmail.com>
2026-03-31 18:30:33 +0600

    i add login and signup design only

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import ErrorPage from "../pages/errorPage/ErrorPage";
 import Home from "../pages/home/Home";
+import Login from "../components/Authentication/Login/Login";
+import SignUp from "../components/Authentication/SignUp/SignUp";
 
 const router = createBrowserRouter([
     {
@@ -13,6 +15,14 @@ const router = createBrowserRouter([
                 path: "/",
                 element: <Home/>
             },
+            {
+                path: "/login",
+                element: <Login/>
+            },
+            {
+                path: "/signup",
+                element: <SignUp/>
+            }
         ]
     }
 ]);
