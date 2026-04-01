ca8a261485491594a4a308ad30a66472143d05ac
mdmahmudulhasan11 <mahmudulhasanfahad96@gmail.com>
2026-04-01 14:04:48 +0600

    feat(todo): add markdown support

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,16 @@
 import Component1 from "./components/component1/component1"
+import Markdown from "./components/markdown/Markdown"
 
 const Home = () => {
   return (
     <div>
       homepage
+      <Markdown/>
       <Component1/>
     </div>
   )
 }
 
 export default Home
+
+
