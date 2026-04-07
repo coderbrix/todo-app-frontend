dac3cd9dad5a567eb0229b458cf9447afe9d5017
rayhankobir-dev <rayhankobir893@gmail.com>
2026-04-07 11:04:43 +0600

    fix: conflicts

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,21 +1,5 @@
-import Component1 from "./components/component1/component1"
-import Markdown from "./components/markdown/Markdown"
-import Layout from "../../components/Layout/Layout"
-import Navbar from "../../components/Layout/Navbar"
-
-
 const Home = () => {
-  return (
-    <div>
-      homepage
-      <Markdown/>
-      <Component1/>
-  
-      <Layout/>
-    </div>
-  )
-}
+  return <div>hello</div>;
+};
 
-export default Home
-
-
+export default Home;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,30 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
-import Main from "../layout/Main";
 import ErrorPage from "../pages/errorPage/ErrorPage";
 import Home from "../pages/home/Home";
+import MainLayout from "../components/Layout/Layout";
 import Login from "../components/Authentication/Login/Login";
 import SignUp from "../components/Authentication/SignUp/SignUp";
 
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
         path: "/",
-        element: <Main/>,
-        errorElement: <ErrorPage/>,
-        children:[
-            {
-                path: "/",
-                element: <Home/>
-            },
-            {
-                path: "/login",
-                element: <Login/>
-            },
-            {
-                path: "/signup",
-                element: <SignUp/>
-            }
-        ]
-    }
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
 ]);
 
 export default router;
