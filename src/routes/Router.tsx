2844fd70e75d0f427babdaafb54b72056e7d7fdf
fazle-rabby <fazlerabby272@gmail.com>
2026-04-08 16:20:05 +0600

    fix: update profile name in Sidebar component

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -4,6 +4,8 @@ import Home from "../pages/home/Home";
 import MainLayout from "../components/Layout/Layout";
 import Login from "../components/Authentication/Login/Login";
 import SignUp from "../components/Authentication/SignUp/SignUp";
+import InboxPage from "../features/inbox/InboxPage";
+import TodayPage from "../features/today/TodayPage";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +18,14 @@ const router = createBrowserRouter([
         element: <Home />,
       },
     ],
+  },
+  {
+    path: "inbox",
+    element: <InboxPage />,
+  },
+  {
+    path: "today",
+    element: <TodayPage />,
   },
   {
     path: "/login",
